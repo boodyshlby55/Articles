c83789d63ff87dc645c22904bec02982d5b915c0
boodyshlby55 <boodyshlby55@gmail.com>
2023-07-23 22:57:00 +0300

    create home

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
+app.set('view engine', 'ejs')
+app.set('views', './views');
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.redirect('/Home')
+})
+
+app.get('/Home', (req, res) => {
+    res.render('index')
+})
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found')
 })
 
 app.listen(port, () => {
