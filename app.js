e684a4bdc7607f93aa7aa064a500a0a632c53eb5
boodyshlby55 <boodyshlby55@gmail.com>
2023-07-24 18:07:05 +0300

    add layout

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const ejsLayout = require('express-ejs-layouts')
+
+dotenv.config();
 const app = express()
-const port = 5000
 app.set('view engine', 'ejs')
 app.set('views', './views');
 app.use(express.static('public'))
+app.use(ejsLayout)
+
+// auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
 
 app.get('/', (req, res) => {
     res.redirect('/Home')
@@ -17,6 +38,6 @@ app.use((req, res) => {
     res.status(404).send('Page Not Found')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${process.env.port}`)
 })
