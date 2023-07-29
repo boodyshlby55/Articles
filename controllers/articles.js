bbb0276dec45ae6c67ba2e51d4c39983ebf3f17c
boodyshlby55 <boodyshlby55@gmail.com>
2023-07-29 20:38:19 +0300

    show articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,7 +1,13 @@
 import Article from "../models/articlesSchema.js"
 
 export const articlesPage = (req, res) => {
-    res.render('articles', { pageTitle: "Articles", display: "d-none" })
+    Article.find()
+        .then((result) => {
+            res.render('articles', { pageTitle: "Articles", display: "d-none", articles: result })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
 
 export const addArticlePage = (req, res) => {
@@ -12,7 +18,7 @@ export const saveArticle = (req, res) => {
     const article = new Article(req.body)
     article.save()
         .then(() => {
-            res.redirect('/home')
+            res.redirect('/articles')
         })
         .catch((err) => {
             console.log(err);
@@ -21,5 +27,5 @@ export const saveArticle = (req, res) => {
     //     await Article.create({
     //         title, details
     //     })
-    //     res.redirect('/home')
+    //     res.redirect('/articles')
 }
